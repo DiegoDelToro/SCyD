2c6a3e8c0e928e1d9a0a8c247de2e65025424896
DiegoDelToro <36480961+DiegoDelToro@users.noreply.github.com>
2018-05-16 00:07:59 -0500

    Icono (imagen)
     El botón que se programó para borrar cada registro, tiene un ícono (imagen) pequeño con una equis.

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,8 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]">X</button>');
+ $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="https://cdn4.iconfinder.com/data/icons/
+       gnome-desktop-icons-png/PNG/32/Gnome-Process-Stop-32.png"></button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
