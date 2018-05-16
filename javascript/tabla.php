d79ff03e400ff1ca8b85d3c335ac461a272cfd9c
DiegoDelToro <36480961+DiegoDelToro@users.noreply.github.com>
2018-05-16 00:15:19 -0500

    Contraseña
    La columna de contraseña no aparece en la tabla.

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,7 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="https://cdn4.iconfinder.com/data/icons/
+ $cols = array('0', '1', '3', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="https://cdn4.iconfinder.com/data/icons/
        gnome-desktop-icons-png/PNG/32/Gnome-Process-Stop-32.png"></button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
